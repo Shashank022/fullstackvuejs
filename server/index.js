1d94d38f45edfc7713f8104883ec4ba54a7dd5cb
Shashank022 <shashank022@gmail.com>
2019-07-28 15:35:31 -0400

    Added file changed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import { json } from 'body-parser';
-import cors from 'cors';
+const express = require('express');
+const bodyParser = require('bodyParser');
+const cors = require('cors');
 
 
 const app = express();
 
-app.arguments(json);
+app.use(bodyParser.json());
 app.use(cors());
 
 const port = process.env.PORT || 5000;
