e9a8c905ff76a4fdaf3f7b41343aabbdd0868bdc
Shashank022 <shashank022@gmail.com>
2019-07-28 23:56:32 -0400

    Added file changed

---
+++ server/routes/api/posts.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+
+const router = express.Router();
+
+//Get Posts
+router.get('/',async (req,res)=> {
+
+const posts = await loadPoostCollection();
+res.send(await posts.find({}).toArray());
+});
+
+
+//Add Posts
+
+router.post('/', async(req,res)=>{
+    const posts = await loadPoostCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+
+
+// Delete Posts
+
+async function loadPoostCollection(){
+    const client = await mongodb.MongoClient.connect
+    ('mongodb://user:group12@ds255917.mlab.com:55917/vue_express',{
+        useNewUrlParser:true
+    });
+
+    return client.db('vue_express').collection('posts');
+}
+
+module.exports = router;
